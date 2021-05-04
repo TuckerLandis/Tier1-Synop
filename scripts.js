472296ee7babb5406c2120ff2f93da62d1e4f39b
TuckerLandis <landistuckerc@gmail.com>
2021-05-04 17:46:09 -0500

    finished snyop

--- scripts.js
+++ scripts.js
@@ -18,7 +18,40 @@ let newPurchase = {
   $( '#purchaseNameIn' ).val( '' );
   $( '#purchasePriceIn' ).val( '' );
   // calculate remaining budget
+  calculateRemainingBudget();
+  //display to DOM
+  displayPurchases();
 }
+
+function calculateRemainingBudget () {
+  console.log('in calculateRemainingBudget');
+  // loop through Purchases
+  let totalPrices = 0;
+  for (i = 0; i < purchases.length; i++) {
+    // add up prices
+    totalPrices += Number( purchases[i].price );
+    // subtract total prices from budget = remaining budgetOut
+  } // end for loop
+console.log('totalPrices:', totalPrices);
+const remainingBudget = budget - totalPrices;
+  //display remaining
+  let el = $( '#remainingBudgetOut' );
+  el.empty();
+  el.append( remainingBudget );
+} // end calculateRemainingBudget
+
+function displayPurchases(){
+  console.log('in displayPurchases');
+  // target output by ID
+  let el = $ ('#purchasesOut');
+  // empty
+  el.empty();
+  // loop through purchases
+  for (let purchase of purchases) {
+    el.append( `<li>` + purchase.name + `: $` + purchase.price + `</li>` );
+  }
+  // for each purchase, create list item
+} // end displayPurchases
 
 function readyNow() {
   let el = $( '#budgetOut' );
@@ -26,5 +59,6 @@ function readyNow() {
   el.append( budget );
 // handle click event
 $( '#addPurchaseButton' ).on( 'click', addPurchase );
-
+// init display
+calculateRemainingBudget();
 } // end readyNow
